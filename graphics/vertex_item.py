0999f34ac8e3efbb3181ce6f1882a8efec7b5653
Maciej Mazurek <mazurekk67@gmail.com>
2025-10-27 21:24:11 +0100

    Projet structure refactoring - VertexItem and ControlPointItem

---
+++ graphics/vertex_item.py
@@ -0,0 +1,50 @@
+from model.model import Vertex
+from PySide6.QtWidgets import (
+    QGraphicsEllipseItem,
+    QGraphicsItem,
+    QMenu,
+)
+from PySide6.QtGui import (
+    QBrush,
+    QColor,
+)
+
+class VertexItem(QGraphicsEllipseItem):
+    def __init__(self, vertex : Vertex, parent=None):
+        # We call the constructor of the base class to create an ellipse item
+        super().__init__(-vertex.radius, -vertex.radius, 
+                         vertex.radius*2, vertex.radius*2, parent)
+        self.vertex = vertex
+        self.setBrush(QBrush(QColor("black")))
+        self.setFlag(QGraphicsItem.GraphicsItemFlag.ItemIsMovable, True)
+        self.setFlag(QGraphicsItem.GraphicsItemFlag.ItemSendsScenePositionChanges, True)
+        # Setting Z value to be on top of edges
+        self.setZValue(2.0)
+
+    # Virtual method which intercepts changes of the item state
+    def itemChange(self, change : QGraphicsItem.GraphicsItemChange, value):
+        # If position of the vertex has changed
+        if change == QGraphicsItem.GraphicsItemChange.ItemPositionHasChanged and self.parentItem():
+            parent = self.parentItem()
+            # We dont inform parent if the position change was caused by parent
+            # itself (to avoid infinite loops) - we only inform parent when
+            # the user drags the vertex directly 
+            if not parent.updating_from_parent:
+                vertex_new_scene_coords = parent.mapToScene(value)
+                parent.on_vertex_moved(self.vertex, vertex_new_scene_coords)
+        return super().itemChange(change, value)
+
+    def contextMenuEvent(self, event):
+        menu = QMenu()
+        del_action = menu.addAction("Delete vertex")
+        sp = event.screenPos()
+        try:
+            qp = sp.toPoint()
+        except Exception:
+            qp = sp
+        chosen_action = menu.exec(qp)
+        if chosen_action == del_action:
+            parent = self.parentItem()
+            if parent:
+                parent.delete_vertex(self.vertex)
+        event.accept()
--- view/items.py
+++ view/items.py
@@ -21,64 +21,8 @@ from PySide6.QtCore import QPointF, QRectF, Qt
 
 import algorithms
 
-class VertexItem(QGraphicsEllipseItem):
-    def __init__(self, vertex : Vertex, parent=None):
-        # We call the constructor of the base class to create an ellipse item
-        super().__init__(-vertex.radius, -vertex.radius, 
-                         vertex.radius*2, vertex.radius*2, parent)
-        self.vertex = vertex
-        self.setBrush(QBrush(QColor("black")))
-        self.setFlag(QGraphicsItem.GraphicsItemFlag.ItemIsMovable, True)
-        self.setFlag(QGraphicsItem.GraphicsItemFlag.ItemSendsScenePositionChanges, True)
-        # Setting Z value to be on top of edges
-        self.setZValue(2.0)
-
-    # Virtual method which intercepts changes of the item state
-    def itemChange(self, change : QGraphicsItem.GraphicsItemChange, value):
-        # If position of the vertex has changed
-        if change == QGraphicsItem.GraphicsItemChange.ItemPositionHasChanged and self.parentItem():
-            parent = self.parentItem()
-            # We dont inform parent if the position change was caused by parent
-            # itself (to avoid infinite loops) - we only inform parent when
-            # the user drags the vertex directly 
-            if not parent.updating_from_parent:
-                vertex_new_scene_coords = parent.mapToScene(value)
-                parent.on_vertex_moved(self.vertex, vertex_new_scene_coords)
-        return super().itemChange(change, value)
-
-    def contextMenuEvent(self, event):
-        menu = QMenu()
-        del_action = menu.addAction("Delete vertex")
-        sp = event.screenPos()
-        try:
-            qp = sp.toPoint()
-        except Exception:
-            qp = sp
-        chosen_action = menu.exec(qp)
-        if chosen_action == del_action:
-            parent = self.parentItem()
-            if parent:
-                parent.delete_vertex(self.vertex)
-        event.accept()
-    
-class ControlPointItem(QGraphicsEllipseItem):
-    def __init__(self, vertex: Vertex, parent=None, color="orange"):
-        super().__init__(-vertex.radius/1.2, -vertex.radius/1.2,
-                         vertex.radius*2/1.2, vertex.radius*2/1.2, parent)
-        self.vertex = vertex
-        self.setBrush(QBrush(QColor(color)))
-        self.setFlag(QGraphicsItem.GraphicsItemFlag.ItemIsMovable, True)
-        self.setFlag(QGraphicsItem.GraphicsItemFlag.ItemSendsScenePositionChanges, True)
-        # Setting Z value to be above vertices and edges
-        self.setZValue(3.0)
-
-    def itemChange(self, change: QGraphicsItem.GraphicsItemChange, value):
-        if change == QGraphicsItem.GraphicsItemChange.ItemPositionHasChanged and self.parentItem():
-            parent = self.parentItem()
-            if not parent.updating_from_parent:
-                control_new_scene_coords = parent.mapToScene(value)
-                parent.on_control_moved(self.vertex, control_new_scene_coords)
-        return super().itemChange(change, value)
+from graphics.vertex_item import VertexItem
+from graphics.controlpoint_item import ControlPointItem
     
 # Base class for edge items (StandardLine, BresenhamLine, Bezier, Arc)
 class EdgeItem(QGraphicsItem):
